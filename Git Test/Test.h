a3f883605c82fa40c22cb867c3278d0729b16f55
Christian Montes <omar91@Christians-MacBook-Pro.local>
2014-09-06 00:21:07 -0400

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Christian Montes on 9/6/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Christian Montes on 9/6/14.
+//
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
